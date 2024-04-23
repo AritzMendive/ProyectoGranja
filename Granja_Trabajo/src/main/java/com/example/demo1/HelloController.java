a0a9c7351416821978bb55b3e0ea79173bd51066
INIGO445 <84372128+INIGO445@users.noreply.github.com>
2024-04-23 21:00:22 +0200

    Add files via upload

--- Granja_Trabajo/src/main/java/com/example/demo1/HelloController.java
+++ Granja_Trabajo/src/main/java/com/example/demo1/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private Button BotonLogin;
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        Alert error = new Alert(Alert.AlertType.ERROR);
+        error.setTitle("Error");
+        error.setContentText("El usuario con el que está intentando logearse no tiene permisos para acceder a la aplicación, contacte con el CAU para poder solucionarlo");
+        error.setHeaderText("Error al Iniciar Sesion");
+        error.show();
+    }
+    @FXML
+    protected void loginButton(){
+        Alert error = new Alert(Alert.AlertType.ERROR);
+        error.setTitle("Error");
+        error.setContentText("El usuario con el que está intentando logearse no tiene permisos para acceder a la aplicación, contacte con el CAU para poder solucionarlo");
+        error.setHeaderText("Error al Iniciar Sesion");
+        error.show();
     }
 }
