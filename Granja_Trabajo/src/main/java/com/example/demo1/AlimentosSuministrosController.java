814a087264734f4f6981728c893f23f58650c047
DanielDeMingo <ddemingsan@educacion.navarra.es>
2024-04-25 19:01:23 +0200

    Creando interfaz para alimentos y suministros del granjero.

---
+++ Granja_Trabajo/src/main/java/com/example/demo1/AlimentosSuministrosController.java
@@ -0,0 +1,10 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AlimentosSuministrosController {
+
+}
