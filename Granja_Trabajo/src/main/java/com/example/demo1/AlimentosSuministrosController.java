3d1a98559bf8d255620984af625be51f6cdc0563
AritzMendive <149406512+AritzMendive@users.noreply.github.com>
2024-04-26 16:18:05 +0200

    Conexión a la base de datos
    Añadido el archivo jar y modificación del archivo sql para que cree la base de datos cuando se ejecute el programa

--- Granja_Trabajo/src/main/java/com/example/demo1/AlimentosSuministrosController.java
+++ Granja_Trabajo/src/main/java/com/example/demo1/AlimentosSuministrosController.java
@@ -4,20 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
-import java.sql.SQLException;
 
 public class AlimentosSuministrosController {
-
-    @FXML
-    private TextField fieldAlimentosSuministros;
-    @FXML
-    private TextField fieldCantidad;
-    @FXML
-    private TextField fieldPrecio;
-    @FXML
-    private Button buttonInsertar;
-
 
 }
--- Granja_Trabajo/src/main/java/com/example/demo1/HelloApplication.java
+++ Granja_Trabajo/src/main/java/com/example/demo1/HelloApplication.java
@@ -5,7 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class HelloApplication extends Application {
     @Override
@@ -17,7 +25,34 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader("src/main/BBDD/granja.sql"));
+
+        try {
+            String st;
+            ArrayList<String> lineas = new ArrayList<>();
+            while((st = br.readLine()) != null){
+                lineas.add(st);
+            }
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "root");
+            Statement statement = connection.createStatement();
+            for(String linea : lineas){
+                statement.executeUpdate(linea);
+            }
+/*
+            ResultSet resultSet = statement.executeQuery("select * from alimentos");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("Nombre"));
+                System.out.println(resultSet.getString("Stock"));
+            }
+*/
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
         launch();
+
     }
 }
