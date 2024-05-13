d75186657a1223335c0fb933cb5f3807ce404b34
AritzMendive <149406512+AritzMendive@users.noreply.github.com>
2024-05-13 16:17:30 +0200

    Corrijo errores
    He corregido algunos errores como la creacion de BBDD

---
+++ ProyectoGranja-main (9)/ProyectoGranja-main/Granja_Trabajo/src/main/java/com/example/demo1/ConexionBBDD.java
@@ -0,0 +1,54 @@
+package com.example.demo1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ConexionBBDD {
+
+    Connection connection;
+    Statement statement;
+
+    public ConexionBBDD() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "root");
+        statement = connection.createStatement();
+    }
+
+
+    public void creacionBBDD() throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader("src/main/BBDD/granja.sql"));
+
+        try {
+            String st;
+            ArrayList<String> lineas = new ArrayList<>();
+            while ((st = br.readLine()) != null) {
+                lineas.add(st);
+            }
+
+
+            for (String linea : lineas) {
+                statement.executeUpdate(linea);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void migracionClienteProveedor() throws SQLException {
+        statement.execute("use granja");
+        statement.execute("CREATE PROCEDURE if not exists ActualizarRoles()\n" +
+                "BEGIN\n" +
+                "    UPDATE Usuarios\n" +
+                "    SET Rol = 'Proveedor'\n" +
+                "    WHERE Rol = 'Cliente';\n" +
+                "END");
+    }
+
+
+
+}
